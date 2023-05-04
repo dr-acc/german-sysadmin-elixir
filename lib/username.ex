b8ed86016b560dff2ace559213936c12032beb46
Your Name <you@example.com>
2023-05-04 15:41:52 -0700

    Updated with mentoring

--- lib/username.ex
+++ lib/username.ex
@@ -1,10 +1,33 @@
 defmodule Username do
-  def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
+  @moduledoc """
+  A module for functions that sanitize an email address from German.
+  """
 
-    # Please implement the sanitize/1 function
+  @doc """
+  A function for sanitizing emails by transforming characters in German to English equivalents.
+  """
+  def sanitize '' do
+    ''
+  end
+
+  @spec sanitize( charlist() ) :: charlist()
+  def sanitize([ char | tail ]) do
+    sanitized =
+      case char do
+      # ä becomes ae
+      ?ä -> 'ae'
+      # ö becomes oe
+      ?ö -> 'oe'
+      # ü becomes ue
+      ?ü -> 'ue'
+      # ß becomes ss
+      ?ß -> 'ss'
+      x when x >= ?a and x <= ?z -> [x]
+      #underscores remain
+      ?_ -> '_'
+      _ -> ''
+    end
+    #recursive call concats sanitized with tail
+    sanitized ++ sanitize(tail)
   end
 end
